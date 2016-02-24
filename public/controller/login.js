a8a04ba71a6c41960160f0598444beaf7064f467
sandeep <sandeepkumarpani888@gmail.com>
2016-02-24 20:53:20 +0530

    Login resolved

--- public/controller/login.js
+++ public/controller/login.js
@@ -1,5 +1,5 @@
 var myApp=angular.module('myApp',[]);
-myApp.controller('AppCtrl',['$scope','$log','$location','$window','$http',function($scope,$http,$log,$window){
+myApp.controller('AppCtrl',['$scope','$http','$window','$log','$location',function($scope,$http,$window,$log,$location){
 	console.log("Hello Wrold from controller");
 
 // var refresh=function(){
@@ -13,9 +13,68 @@ myApp.controller('AppCtrl',['$scope','$log','$location','$window','$http',functi
 //refresh();
 
 $scope.studentLogin=function(){
-	console.log("hi there");
-	var url="/student.html";
-	$window.location.href=url;
+	var type=$scope.person.type;
+	if(type==1){
+		var student={'email':$scope.person.email,'password':$scope.person.password};
+		console.log(student);
+		$http.post('/student/login',student).success(function(response){
+			if(response==1){
+				var url="/student.html";
+				console.log("Authentication successful");
+				$window.location.href=url;
+				console.log("Authnetication successfull");
+			}
+			else{
+				console.log("Authentication failed");
+			}
+		})
+	}
+	else if(type==2){
+		var faculty={'email':$scope.person.email,'password':$scope.person.password};
+		console.log(faculty);
+		$http.post('/faculty/login',faculty).success(function(response){
+			if(response==1){
+				console.log("Authnetication success");
+				var url="/student.html";
+				console.log("Authentication successful");
+				$window.location.href=url;
+				console.log("Authnetication successfull");
+			}
+			else{
+				console.log("Authentication failed");
+			}
+		})
+	}
+	else if(type==3){
+		var parent={'email':$scope.person.email,'password':$scope.person.password};
+		console.log(parent);
+		$http.post('/parent/login',parent).success(function(response){
+			if(response==1){
+				var url="/parent.html";
+				console.log("Authentication successful");
+				$window.location.href=url;
+				console.log("Authnetication successfull");
+			}
+			else{
+				console.log("Authentication failed");
+			}
+		})
+	}
+	else if(type==4){
+		var admin={'email':$scope.person.email,'password':$scope.person.password};
+		console.log(admin);
+		$http.post('/admin/login',admin).success(function(response){
+			if(response==1){
+				var url="/admin.html";
+				console.log("Authentication successful");
+				$window.location.href=url;
+				console.log("Authnetication successfull");
+			}
+			else{
+				console.log("Authentication failed");
+			}
+		})
+	}
 }
 
 // $scope.addContact=function(){
@@ -48,4 +107,12 @@ $scope.studentLogin=function(){
 // 	$http.put('/contactlist/'+id,$scope.contact);
 // }
 
+}])
+
+myApp.controller('getType',['$scope','$log','$location','$window','$http',function($scope,$http,$log,$window){
+	console.log("Hello Wrold from setType");
+
+$scope.setType=function(type){
+	$scope.person.type=type;
+}
 }])
--- server.js
+++ server.js
@@ -1,19 +1,53 @@
 var express=require('express');
 var mongojs=require('mongojs');
 var bodyParser=require('body-parser');
-var db=mongojs('student',['student']);
+var dbStudent=mongojs('student',['student']);
+var dbParent=mongojs('parent',['parent']);
+var dbFaculty=mongojs('faculty',['faculty']);
+var dbAdmin=mongojs('admin',['admin']);
 var app=express();
 
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
+console.log("Hello World from Server");
 
 app.post('/student/login',function(req,res){
 	console.log('Got a POST request')
 	console.log(req.body);
-	db.student.find(req.body).count(function(err,docs){
+	dbStudent.student.find(req.body).count(function(err,docs){
 		var studentAuthenticationStatus=docs;
 		console.log(studentAuthenticationStatus);
 		res.json(studentAuthenticationStatus);
+	});
+})
+
+app.post('/faculty/login',function(req,res){
+	console.log('Got a POST request')
+	console.log(req.body);
+	dbFaculty.faculty.find(req.body).count(function(err,docs){
+		var facultyAuthenticationStatus=docs;
+		console.log(facultyAuthenticationStatus);
+		res.json(facultyAuthenticationStatus);
+	});
+})
+
+app.post('/parent/login',function(req,res){
+	console.log('Got a POST request')
+	console.log(req.body);
+	dbParent.parent.find(req.body).count(function(err,docs){
+		var parentAuthenticationStatus=docs;
+		console.log(parentAuthenticationStatus);
+		res.json(parentAuthenticationStatus);
+	});
+})
+
+app.post('/admin/login',function(req,res){
+	console.log('Got a POST request')
+	console.log(req.body);
+	dbAdmin.admin.find(req.body).count(function(err,docs){
+		var adminAuthenticationStatus=docs;
+		console.log(adminAuthenticationStatus);
+		res.json(adminAuthenticationStatus);
 	});
 })
 
